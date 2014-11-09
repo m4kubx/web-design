0f79718ff7b8d48753a577e365fa4314739326e2
m4kubx <songo22@gmail.com>
2014-11-08 19:37:12 -0430

    Actualización del proyecto Muestrario

--- Muestrario/js/main.js
+++ Muestrario/js/main.js
@@ -1,5 +1,28 @@
 $(document).on("ready", inicio);
 
 function inicio () {
-	
+	$("#personalizar").on("click", transicion);	
+}
+
+function transicion() {
+	var cambiosCSS = {
+		margin: 0,
+		overflow: "hidden",
+		padding: 0,
+		width: 0
+	};
+	var cambiosPerson = {
+		height: "auto",
+		opacity: 1,
+		width: "35%"
+	};
+	$("#historia").css(cambiosCSS);
+	$("#personalizacion").css(cambiosPerson);
+
+	$("#color div").on("click", cambiarColor);
+}
+
+function cambiarColor(datos) {
+	var color = datos.currentTarget.id;
+	$("#imagen img").attr("src","./images/c"+color+".jpg");
 }
